ded69bcfad80a7a99bb4d39d4b9fa0f949b3392d
Honza <pokorny.jan.94@gmail.com>
2017-09-30 18:20:08 +0200

    No sinus just rect a circle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const getCanvas = () => document.getElementById("canvas");
 const setupCanvas = canvas => {
   const canvasCtx = getCanvas().getContext("2d");
+  const width = document.body.clientWidth;
+  const height = document.body.clientHeight;
+  canvas.width = width;
+  canvas.height = height;
   canvasCtx.fillStyle = "black";
-  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  canvasCtx.fillRect(0, 0, width, height);
 };
 
 function* getSinusPairs(count, amplitude, freq, faze = 0, step = 1) {
@@ -28,7 +32,6 @@ const drawSinus = (
   freq,
   faze = 0,
 ) => {
-  console.log(strokeConf, possitionCords, amplitude, freq);
   const canvasCtx = canvas.getContext("2d");
   const sinusPairGen = getSinusPairs(canvas.width, amplitude, freq, faze);
   canvasCtx.beginPath();
@@ -52,38 +55,49 @@ const drawSinus = (
 
   canvasCtx.stroke();
 };
+
+const drawCircle = (canvas, strokeConf, possitionCords, radius) => {
+  const canvasCtx = canvas.getContext("2d");
+  canvasCtx.beginPath();
+  canvasCtx.strokeStyle = strokeConf.strokeStyle;
+  canvasCtx.lineWidth = strokeConf.lineWidth;
+  canvasCtx.fillStyle = strokeConf.fillStyle;
+  canvasCtx.arc(possitionCords.x, possitionCords.y, radius, 0, 2 * Math.PI);
+  canvasCtx.fill();
+  canvasCtx.stroke();
+};
+
+const drawSquare = (canvas, strokeConf, possitionCords, widius) => {
+  const canvasCtx = canvas.getContext("2d");
+  canvasCtx.beginPath();
+  canvasCtx.strokeStyle = strokeConf.strokeStyle;
+  canvasCtx.lineWidth = strokeConf.lineWidth;
+  canvasCtx.fillStyle = strokeConf.fillStyle;
+  canvasCtx.rect(possitionCords.x, possitionCords.y, widius, widius);
+  canvasCtx.fill();
+  canvasCtx.stroke();
+};
+
 const run = () => {
   const canvas = getCanvas();
   setupCanvas(canvas);
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 500; i += 1) {
     const strokeConf = {
       strokeStyle: getRandomColor(),
-      lineWidth: Math.random() * 8,
+      lineWidth: Math.random() * 15,
+      fillStyle: getRandomColor(),
     };
     const possitionCords = {
-      x: 0,
+      x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
     };
-    const freq = Math.random() / 100;
-    const amplitude = Math.random() * canvas.height;
-    drawSinus(canvas, strokeConf, possitionCords, amplitude, freq);
+    const radius = Math.random() * canvas.height / 10.2;
+    if (Math.random() < 0.3) {
+      drawSquare(canvas, strokeConf, possitionCords, radius);
+    } else {
+      drawCircle(canvas, strokeConf, possitionCords, radius);
+    }
   }
-  /*
-  drawSinus(
-    canvas,
-    { strokeStyle: "red", lineWidth: 6 },
-    { x: 0, y: 90 },
-    90,
-    0.003,
-  );
-  drawSinus(
-    canvas,
-    { strokeStyle: "blue", lineWidth: 12 },
-    { x: 0, y: 190 },
-    90,
-    0.003,
-  );
-  */
 };
 
 run();
